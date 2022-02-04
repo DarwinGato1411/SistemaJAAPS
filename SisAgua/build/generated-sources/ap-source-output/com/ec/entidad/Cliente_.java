6dc36fb43db777de24cf4211338492127b5b8302
DarwinGato1411 <darwinvinicio14@gmail.com>
2022-02-03 23:07:44 -0500

    cambios Danny

--- SisAgua/build/generated-sources/ap-source-output/com/ec/entidad/Cliente_.java
+++ SisAgua/build/generated-sources/ap-source-output/com/ec/entidad/Cliente_.java
@@ -10,7 +10,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-02-03T11:51:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-02-03T21:47:50")
 @StaticMetamodel(Cliente.class)
 public class Cliente_ { 
 
--- SisAgua/build/generated-sources/ap-source-output/com/ec/entidad/TipoRetencion_.java
+++ SisAgua/build/generated-sources/ap-source-output/com/ec/entidad/TipoRetencion_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-02-03T11:51:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-02-03T21:47:50")
 @StaticMetamodel(TipoRetencion.class)
 public class TipoRetencion_ { 
 
